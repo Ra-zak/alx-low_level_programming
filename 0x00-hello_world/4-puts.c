16f13fff7a54f4649ae6f2db5d0d9b893a3e22c3
ra-zak <razak.aa74@gmail.com>
2022-10-13 05:31:27 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
-/*print a string using puts*/
+
+
+/**
+ *   * main - Entry point
+ *
+ *   *
+ *
+ *   * Return: Always 0 (Success)
+ *
+ *     */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
