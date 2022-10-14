235cb1cdcac0244d1a5b0fb4ec7055cc9b3a762b
ra-zak <razak.aa74@gmail.com>
2022-10-14 14:00:51 -0700

    jj

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * *
+ * Return: 0 if success
+ * *
+*/
+int main(void)
+{
+	char x;
+
+	x = 'z';
+	while(x >= 'a')
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
