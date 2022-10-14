085e67d31130287d6ebceb2a0fe7d90e098dbb0c
ra-zak <razak.aa74@gmail.com>
2022-10-14 15:53:24 -0700

    kk

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,20 @@
 */
 int main(void)
 {
+	int x;
+	int y;
+
 	for (x = 0; x <= 9; x++)
 	{
-		putchar((x % 10) + '0');
-		if (x == 9)
+		for (y = x + 1; y < 10; y++)
 		{
-			continue;
+			putchar((x % 10) + '0');
+			putchar((y % 10) + '0');
+			if (x == 8 && y == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
-		putchar(',');
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
