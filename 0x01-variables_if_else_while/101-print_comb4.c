7002399d9c7f8573a21cc2fecf2df01c65700656
ra-zak <razak.aa74@gmail.com>
2022-10-14 16:42:53 -0700

    ll

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 				putchar((x % 10) + '0');
 				putchar((y % 10) + '0');
 				putchar((z % 10) + '0');
-				if (x == 7 && y == 8 && z == 9)
+				if (x != y && x != z && y != z)
 					continue;
 				putchar(',');
 				putchar(' ');
