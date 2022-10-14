03b5e56ac9be23d2b6cbddadbfc5848026ecd305
ra-zak <razak.aa74@gmail.com>
2022-10-14 15:29:59 -0700

    oh boy

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,15 @@ int main(void)
 	int x;
 
 	x = 0;
-	while (x < 100)
+	for (x <= 9; x++)
 	{
-		putchar((x / 10) + '0');
 		putchar((x % 10) + '0');
-		if (x < 99)
+		if (x == 9)
 		{
-			putchar(',');
-			putchar(' ');
+			continue;
 		}
-		x++;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
