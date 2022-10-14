572f4e6773d25ee95528e8911b6772165a3711a0
ra-zak <razak.aa74@gmail.com>
2022-10-14 13:26:52 -0700

    iggg

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *  *
+ *  Return: 0 if success
+*/
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while (x <= 9)
+	{
+		printf("%d\n", x);
+		x++;
+	}
+	return (0);
+}
