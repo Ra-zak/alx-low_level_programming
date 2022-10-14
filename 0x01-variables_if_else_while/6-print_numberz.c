d227745e8cc0dc6ef0fb4b325e831e04ac9d74e1
ra-zak <razak.aa74@gmail.com>
2022-10-14 13:39:21 -0700

    gg

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ * *
+ * Return:0 if success
+ * *
+*/
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while (x <= 9)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n')
+		return (0);
+}
