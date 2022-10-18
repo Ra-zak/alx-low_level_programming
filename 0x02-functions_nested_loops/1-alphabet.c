555e19d4740b46cc5e8e1594f75bced55c6c905e
ra-zak <razak.aa74@gmail.com>
2022-10-18 13:08:08 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print all the alphabet
+*/
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x < z)
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
