0fb911b1aedef4900ccce5d39eb36ee9daf67b19
ra-zak <razak.aa74@gmail.com>
2022-10-18 07:18:08 -0700

    printing using _putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ * *
+ * Return: 0 if success
+ * *
+*/
+int main(void)
+{
+	char *var = "_putchar";
+
+	while  (*var)
+	{
+		_putchar(*var);
+		*var++;
+	}
+	_putchar('\n');
+	return (0);
+}
