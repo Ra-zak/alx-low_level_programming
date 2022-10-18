50e7e7f025c90c3552875171d22118ede2c177d9
ra-zak <razak.aa74@gmail.com>
2022-10-18 16:02:28 -0700

    hh

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,16 +9,13 @@ void jack_bauer(void)
 
 	for (x = 0; x <= 23; x++)
 	{
-		for (y = x + 1; y <= 59; y++)
+		for (y = 0; y <= 59; y++)
 		{
 			_putchar((x / 10) + '0');
 			_putchar((x % 10) + '0');
 			_putchar(':');
 			_putchar((y / 10) + '0');
 			_putchar((y % 10) + '0');
-
-			if (x == 23 && y == 59)
-				continue;
 			_putchar('\n');
 		}
 	}
