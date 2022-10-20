14cec4052af05f688b3be4904565431f6f1fe5d4
ra-zak <razak.aa74@gmail.com>
2022-10-20 08:08:48 -0700

    most num

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print digit
+*/
+void print_most_numbers(void)
+{
+	char x = '0';
+
+	while (x <= '9')
+	{
+		if (x != '2' || x != '4')
+		{
+			_putchar(x);
+		}
+		x++;
+	}
+	_putchar('\n');
+}
