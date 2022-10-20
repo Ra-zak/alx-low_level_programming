8c828fc41091f7227e2c6f65ed79ae2bfb411984
ra-zak <razak.aa74@gmail.com>
2022-10-20 15:25:38 -0700

    kk

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,13 @@ void print_line(int n)
 {
 	int x = 0;
 
-	while (x <= n)
-	{
-		if (n <= 0)
+		if (n > 0)
 		{
-			_putchar('\n');
+			while (x <= n)
+			{
+				_putchar('_');
+				x++;
+			}
 		}
-		_putchar('_');
-		x++;
-	}
-	_putchar('\n');
+		_putchar('\n');
 }
