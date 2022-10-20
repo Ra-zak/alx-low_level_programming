f1031a6026c95980c129d44145ca7920f3ab6fde
ra-zak <razak.aa74@gmail.com>
2022-10-20 07:15:10 -0700

    update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - entry point
+ * @c: place holder for var
+ * Return: 1 if success 0 if otherwise
+*/
+int _isupper(int c)
+{
+	int c;
+
+	if (c > 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
