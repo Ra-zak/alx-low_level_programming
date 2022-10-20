65e67a80a8d1b313dc27bf79a720d0b02bd1acf0
ra-zak <razak.aa74@gmail.com>
2022-10-20 16:07:15 -0700

    jj

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *  print_diagonal -entry
+ *  @n: place holder
+*/
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n > 0)
+	{
+		x = 0;
+		while (x < n)
+		{
+			y = 0;
+			while (y < x)
+			{
+				_putchar(' ');
+				y++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			x++;
+		}
+	}
+	_putchar('\n');
+}
