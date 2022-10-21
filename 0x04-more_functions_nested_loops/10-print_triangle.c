f0b0df62d2b35d50a8dce1b4a1bf83ac4ba83001
ra-zak <razak.aa74@gmail.com>
2022-10-21 16:33:00 -0700

    up

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle -entry to making triangle
+ * @size: size of triangle
+*/
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = size; y >= 1; y--)
+			{
+				if (x < y)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
