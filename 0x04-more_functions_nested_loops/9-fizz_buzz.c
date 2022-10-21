a827ebccebeab903dec31164aa8efbfe158f56c2
ra-zak <razak.aa74@gmail.com>
2022-10-21 14:50:12 -0700

    com

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0 always true
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 101; x++)
+	{
+		if (x % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	return (0);
+}
