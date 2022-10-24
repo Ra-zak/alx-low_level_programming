1f4629865af2844efcb2b2f8ef7a9447ea2dd1bc
ra-zak <razak.aa74@gmail.com>
2022-10-24 13:06:48 -0700

    rev str

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - entry to rev str
+ * @s: str to be rev
+*/
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	for (x -= 1; x >= 0; x--)
+	{
+		_putchar(s[x]);
+	}
+	_putchar('\n');
+}
