f07d7435dbd506c6a8c246963ae9e086efd920d6
ra-zak <razak.aa74@gmail.com>
2022-11-02 07:04:21 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - checks for the squareroot of a number
+ * @n: number being checked
+ * @x: number to check for against base which is n
+ * Return: square root
+*/
+int help(x, base)
+{
+	if (x * x == base)
+	{
+		return (x);
+	}
+	if (x * x > base)
+	{
+		return (-1);
+	}
+	return (help(x + 1, base));
+}
+int _sqrt_recursion(int n)
+{
+	if ( n == 0)
+	{
+		return (0);
+	}
+	return (help(1, n));
+}
