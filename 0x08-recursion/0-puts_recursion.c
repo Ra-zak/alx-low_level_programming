51bd87721eabea7080692b81af41e2c12949fe6c
ra-zak <razak.aa74@gmail.com>
2022-11-02 04:08:18 -0700

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,12 @@
 */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(s[i]);
-	i++;
-	_puts_recursion(s[i]);
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
