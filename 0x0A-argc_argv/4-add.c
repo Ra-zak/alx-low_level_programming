a108d8a7f8418e5e9f42205fb5df712e74ddbc83
ra-zak <razak.aa74@gmail.com>
2022-11-04 05:43:31 -0700

    ip

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,15 +9,15 @@
  * @argv: vector
  * @i: argc counter
  * @y: argv counter
- * REturn: 0 if successi
+ * Return: 0 if successi
 */
 int help(int argc, int i, unsigned int y, char *argv[])
 {
 	for (i = 1; i <= argc; i++)
 	{
-		for (y = 0; *argv[i] != '\0' && y < strlen(argv[i]); y++)
+		for (y = 0; *argv[i] != '\0' && y < strlen(*argv[i]); y++)
 		{
-			if (isdigit(argv[i][y]) == 0)
+			if (isdigit(*argv[i][y]) == 0)
 			{
 				return (1);
 			}
