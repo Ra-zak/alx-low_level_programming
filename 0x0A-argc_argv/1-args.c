2236a0200f18fd971ff9d6060c9e3d0d19a468ea
ra-zak <razak.aa74@gmail.com>
2022-11-04 04:02:22 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints argc
+ * @argc: number of arguments passed to program
+ * @argv: array conting argc
+ * Return: 0 if success
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
