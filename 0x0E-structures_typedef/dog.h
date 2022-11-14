cff57d04e75a6cdd95615fff18b7a816f4826e21
ra-zak <razak.aa74@gmail.com>
2022-11-14 04:47:18 -0800

    task 0 fixing

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,6 +5,7 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ *
  * Description: Defines data about the dog and type of dog's struct
 */
 struct dog
@@ -14,8 +15,4 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
