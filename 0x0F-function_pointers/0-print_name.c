f9dfde5f5cb75dc0900e528865a006319c76b18d
ra-zak <razak.aa74@gmail.com>
2022-11-16 04:56:11 -0800

    redoing task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,13 @@
 #include "function_pointers.h"
+
 /**
- * print_name - prints a name passed to it
- * @name: input name to print
- * @f: function pointer pointing to char pointer
+ * print_name - Prints a name
+ * @name: The name to prints
+ * @f: Pointer to function
+ * Return: Nothing
 */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-	f(name);
+		f(name);
 }
